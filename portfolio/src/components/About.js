a29b08d1bf475ea3a7d2e541440c77f11307fbfa
Long Quan Nguyen <imq026@usask.ca>
2024-01-11 14:22:09 -0600

    Init react project

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>This is where you would put information about yourself.</p>
+    </div>
+  );
+}
+
+export default About;
