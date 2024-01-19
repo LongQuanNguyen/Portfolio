3779f23b447c2f65c2cb5db8e7e587cf37c4916f
Long Quan Nguyen <imq026@usask.ca>
2024-01-19 12:43:48 -0600

    Fix mobile display for Hoem, About, Contact

--- src/components/About.js
+++ src/components/About.js
@@ -28,17 +28,15 @@ const About = () => {
                             I love the idea of how numbers and logic translated into the key drivers of today's lives, 
                             in digitalized form. I am passionate about acquiring and sharing my knowledge with others, with the hope 
                             that good impacts can be realized.
-                            <br/>
-                            <br/>
-                            Aside from academics, I am an amateur photographer, a book lover, a sports enjoyer and a ponderer. 
-                            <br/>Ever since coming to Canada in 2019, I have developed a strong liking for the local nature. 
+                            <br/><br/>Aside from academics, I am an amateur photographer, a book lover, a sports enjoyer and a ponderer. 
+                            <br/>&emsp;Ever since coming to Canada in 2019, I have developed a strong liking for the local nature. 
                             I adore the patterns in the natural seasons, and I am even more amazed when the patterns sometimes get broken, 
                             exhibiting stochastic features of life; which motivated me to capture these moments through the lens of 
                             the camera.
-                            <br/>Picking up English is the most rewarding decision I ever made. It doubled the amount of books I could read 
-                            and somehow tripled the amount of chaos and confusion, perfectly aligning my insatiable curiosity. 
-                            Novels and narratives, with immersive worlds and perspectives, captivate me the most.
-                            <br/>And then there is sport, who would not love the adrenaline rush? The hype, the clutch shots, the swift and 
+                            <br/>&emsp;Picking up English is the most rewarding decision I ever made. It doubled the amount of books I could read 
+                            and quadruple the amount of confusions (4 combination of confusions: vi-vi, en-en, vi-en, en-vi). This is perfectly 
+                            aligning my insatiable curiosity. Novels and narratives, with immersive worlds and perspectives, captivate me the most.
+                            <br/>&emsp;And then there is sport, who would not love the adrenaline rush? The hype, the clutch shots, the swift and 
                             seamless movements, are all encapsulated in the world of basketball. It is the best sport indoors when 
                             outside is cold and the best sport outdoors when outside is not cold. I also engage in other sports 
                             (mostly due to the lack of basketball companions or I am forced to join). 
